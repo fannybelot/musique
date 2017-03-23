40b7536aa8ca6792eb8562b6c0c2e7f41ce127b0
Fanny <fanny.belot@laposte.net>
2017-03-23 14:44:11 +0200

    init

---
+++ Musique/Musique/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Musique.Startup))]
+namespace Musique
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
