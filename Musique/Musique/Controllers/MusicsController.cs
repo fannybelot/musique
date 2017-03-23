f1ed33a773a3b0440f955218c3bd416a88634a8d
Fanny <fanny.belot@laposte.net>
2017-03-23 15:47:32 +0200

    controller + model init

---
+++ Musique/Musique/Controllers/MusicsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Musique.Models;
+
+namespace Musique.Controllers
+{
+    public class MusicsController : Controller
+    {
+        private MusicDBContext db = new MusicDBContext();
+
+        // GET: Musics
+        public ActionResult Index()
+        {
+            return View(db.Musics.ToList());
+        }
+
+        // GET: Musics/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Music music = db.Musics.Find(id);
+            if (music == null)
+            {
+                return HttpNotFound();
+            }
+            return View(music);
+        }
+
+        // GET: Musics/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Musics/Create
+        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+        // plus de détails, voir  https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Title,ReleaseDate,Artist,Album,Genre,Price")] Music music)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Musics.Add(music);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(music);
+        }
+
+        // GET: Musics/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Music music = db.Musics.Find(id);
+            if (music == null)
+            {
+                return HttpNotFound();
+            }
+            return View(music);
+        }
+
+        // POST: Musics/Edit/5
+        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+        // plus de détails, voir  https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Title,ReleaseDate,Artist,Album,Genre,Price")] Music music)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(music).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(music);
+        }
+
+        // GET: Musics/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Music music = db.Musics.Find(id);
+            if (music == null)
+            {
+                return HttpNotFound();
+            }
+            return View(music);
+        }
+
+        // POST: Musics/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Music music = db.Musics.Find(id);
+            db.Musics.Remove(music);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
